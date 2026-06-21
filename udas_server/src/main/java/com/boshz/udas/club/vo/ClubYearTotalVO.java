3aa7c26d92c6a62ec146948cb857c562dd3cce95
Neil-code-udas <cuiyinan1998@163.com>
2026-06-21 21:08:14 +0800

    111

--- udas_server/src/main/java/com/boshz/udas/club/vo/ClubYearTotalVO.java
+++ udas_server/src/main/java/com/boshz/udas/club/vo/ClubYearTotalVO.java
@@ -1,14 +1,18 @@
 package com.boshz.udas.club.vo;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 /**
  * 接口顶层返回对象
  */
+@Data
 public class ClubYearTotalVO {
     // 查询年度
     private String year;
+
     // 所有俱乐部台账列表
     private List<ClubItemVO> clubList;
     // 全年度总收入合计
@@ -19,53 +23,4 @@ public class ClubYearTotalVO {
     private BigDecimal allSiteFeeTotal;
     // 全年度总结余
     private BigDecimal allSurplusTotal;
-
-    // getter、setter
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public List<ClubItemVO> getClubList() {
-        return clubList;
-    }
-
-    public void setClubList(List<ClubItemVO> clubList) {
-        this.clubList = clubList;
-    }
-
-    public BigDecimal getAllIncomeTotal() {
-        return allIncomeTotal;
-    }
-
-    public void setAllIncomeTotal(BigDecimal allIncomeTotal) {
-        this.allIncomeTotal = allIncomeTotal;
-    }
-
-    public BigDecimal getAllOutTotal() {
-        return allOutTotal;
-    }
-
-    public void setAllOutTotal(BigDecimal allOutTotal) {
-        this.allOutTotal = allOutTotal;
-    }
-
-    public BigDecimal getAllSiteFeeTotal() {
-        return allSiteFeeTotal;
-    }
-
-    public void setAllSiteFeeTotal(BigDecimal allSiteFeeTotal) {
-        this.allSiteFeeTotal = allSiteFeeTotal;
-    }
-
-    public BigDecimal getAllSurplusTotal() {
-        return allSurplusTotal;
-    }
-
-    public void setAllSurplusTotal(BigDecimal allSurplusTotal) {
-        this.allSurplusTotal = allSurplusTotal;
-    }
 }
